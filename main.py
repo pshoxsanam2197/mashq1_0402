95c4e44549d6a0e75d7a053b0c7ad80cca157bae
pshoxsanam2197 <pshoxsanam2197@gmail.com>
2026-02-04 15:32:01 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# 1.1-masala
+numbers = [10, 20, 30, 40, 50,]
+print(len(numbers))
+print(numbers[2])
+print(30 in numbers)
+print(numbers[-2:])
+print(sum(numbers))
+
+# 1.2-masala
+fruits = ['apple', 'banana', 'cherry']
+fruits.append('kivi')
+print(fruits)
+fruits.remove('banana')
+print(fruits)
+fruits.insert(2, 'peach')
+print(fruits)
+fruits.sort()
+print(fruits)
+print(fruits)
